705e889b717671ec49cec20913e518a9fbe3a441
Barış Taş <baristas219@gmail.com>
2023-02-10 12:42:31 +0300

    Added my project and Frontend Mentor Challange

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,24 +3,21 @@ fetch("https://api.github.com/users/tasbaris/repos")
     return response.json();
   })
   .then(function (repos) {
-    let output = "<ul>";
-
+    let count = 0;
     repos.forEach(function (repo) {
-      output += "<li>Repo Name : " + repo.name + "</li>";
-      output += "<ul>";
-      output +=
-        "<li>Language : " + ((repo.language == null) ? "JSON" : repo.language)
-        + "</li>";
-      output +=
-        '<li>Link : <a target="_blank" href="' +
-        repo.html_url +
-        '">Go To Project</a></li>';
-      output += "</ul>";
+      count+=1;
+    let output = '<div class="repo-card">';
+      output += '<img class="repo-img" src="./assets/img/logo.webp" alt="Repo-Image">';
+      output += '<div class="repo-info">';
+      output += '<h3 class="repo-name">'+repo.name+'</h3>';
+      output += '<p class="repo-description">'+repo.description+'</p>';
+      output += '</div>';
+      output += '<a target="_blank" href="'+repo.html_url+'" class="repo-link"><i class="fa-sharp fa-solid fa-circle-right"></i></a>';
+      output += "</div>";
+      document.getElementById("repos").innerHTML += output;
     });
 
-    output += "</ul>";
-
-    //document.getElementById("projects").innerHTML += output;
+    document.querySelector(".repo-count").textContent=count.toString()+" adet bulundu!";
   });
 
 window.addEventListener("scroll", function () {
